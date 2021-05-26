639a88fdf81a6c68ba2bb3831ba2158198084681
Artem Safin <artemsafin.webdev@gmail.com>
2021-05-26 20:01:51 +0300

    Правки

--- src/js/educationSlider.js
+++ src/js/educationSlider.js
@@ -1,11 +1,18 @@
 import { Swiper, Navigation } from 'swiper';
 
+
 Swiper.use([Navigation]);
 export default function educationSlider() {
     const elements = Array.from(document.querySelectorAll('.js-education-school-slider'));
 
     elements.forEach(element => {
         const container = element.querySelector('.swiper-container');
+        const wrapper = element.querySelector('.swiper-container');
+        const slides = Array.from(element.querySelectorAll('.swiper-slide'));
+
+        if (slides.length <= 2) {
+            slides.forEach(slide => wrapper.appendChild(slide.cloneNode(true)));
+        }
 
         new Swiper(container, {
             slidesPerView: 1,
